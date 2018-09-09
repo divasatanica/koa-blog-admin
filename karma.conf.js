fa337e5117a7e05873d5161bdbd36404f87ba636
divasatanica <jayhebes@vip.qq.com>
2018-09-09 21:40:55 +0800

    update: 更新chrome的沙箱配置

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeNoSandbox'],
+
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
 
     // Continuous Integration mode
